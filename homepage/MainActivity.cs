c97d90eb198f3d61ef474245b4dafedd31f0d8c1
visualstudio <visualstudio@122-PC35>
2019-01-31 09:45:43 -0500

    commit

--- homepage/MainActivity.cs
+++ homepage/MainActivity.cs
@@ -18,11 +18,7 @@ namespace homepage
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
